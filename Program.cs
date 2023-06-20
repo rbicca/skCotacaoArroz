22ca5eac6d00ffb91a05685a022d04bc6a65e425
Ronaldo Melchiades Bicca <ronaldo.bicca@gmail.com>
2023-06-19 22:39:27 -0300

    Criação de um serviço base para teste

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-using skCotacaoArroz;
+//Artigo base
+//https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service?pivots=dotnet-7-0
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
 
+using skCotacaoArroz.WindowsService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = "Serviço SK - Cotação do Arroz";
+});
+
+LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+builder.Services.AddSingleton<JokeService>();
+builder.Services.AddHostedService<EsalqueService>();
+
+// See: https://github.com/dotnet/runtime/issues/47303
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging"));
+
+IHost host = builder.Build();
 host.Run();
